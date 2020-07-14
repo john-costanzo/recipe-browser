4bd527e16803f2c0f77f873e1421ccb8f3541d28
John Costanzo <John.Costanzo@dowjones.com>
2020-07-13 21:31:12 -0400

    When hiding an element, also hide its corresponding toc entry

--- search.js
+++ search.js
@@ -1,10 +1,35 @@
+// Turn off display for element with ID.
 function hideElement( id ) {
-    console.log( "\t\t\thide element with id=[%s]", id );
-    document.getElementById( id ).style.display = "none";
+    // console.log( "\t\t\thide element with id=[%s]", id );
+    const el = document.getElementById( id );
+    if( el ) {
+	el.style.display = "none";
+    } else {
+	console.log( "no element with id=[%s]", id );
+    }
 }
+
+// Turn off display for element with ID and its related ids.
+function hideElements( id ) {
+    hideElement( id );
+    hideElement( id + '-toc' );
+}
+
+// Turn on display for element with ID. Use block display.
 function showElement( id ) {
-    console.log( "\t\t\tshow element with id=[%s]", id );
-    document.getElementById( id ).style.display = "block";
+    // console.log( "\t\t\tshow element with id=[%s]", id );
+    const el = document.getElementById( id );
+    if( el ) {
+	el.style.display = "block";
+    } else {
+	console.log( "no element with id=[%s]", id );
+    }
+}
+
+// Turn on display for element with ID and its related ids.
+function showElements( id ) {
+    showElement( id );
+    showElement( id + '-toc' );
 }
 
 // Return true if all of the terms in the array QUERY
@@ -23,20 +48,21 @@ function matchesAll( query, str ) {
 // QUERY is an array of strings.
 // INDEX is an array of arrays: [ id, index_string ].
 // Call hideElement for all elements with an id from INDEX where *all* of the strings
-// in the QUERY array may be found as regular expressions in the corresponding index_string. Else call showElement for those ids.
+// in the QUERY array may be found as regular expressions in the corresponding index_string.
+// Else call showElement for those ids.
 function searchElements( query, index ) {
     let result = []
-    console.log( "searchElements: index=[%s]\tquery=[%s]", index.join(", "), query.join(", ") );
+    // console.log( "searchElements: index=[%s]\tquery=[%s]", index.join(", "), query.join(", ") );
 
     indexLoop:
     for (const idx of index ) {
 	const id = idx[0];
 	const index_string = idx[1];
-	console.log( "\tid=[%s]\tindex_string=[%s]", id, index_string );
+	// console.log( "\tid=[%s]\tindex_string=[%s]", id, index_string );
 	if( matchesAll( query, index_string ) ) {
-	    showElement( id );
+	    showElements( id );
 	} else {
-	    hideElement( id );
+	    hideElements( id );
 	}
     }
 }
@@ -45,10 +71,10 @@ function searchElements( query, index ) {
 // Use the query in the query box to filter the page according to the recipeIndex
 function keyboardHandler(e) {
     const key = String.fromCharCode( e.which || e.keyCode ).toLowerCase();
-    console.log( "[%s] pressed", key );
+    //console.log( "[%s] pressed", key );
     let query = document.getElementById( "searchBox" ).value;
     query = query.replace( /[^0-9a-z ]+/g, "" )
-    console.log( "query=[%s]", query );
+    //console.log( "query=[%s]", query );
     searchElements( query.split( " " ), recipeIndex );
 }
 
@@ -58,12 +84,15 @@ document.getElementById( "searchBox" ).
 //==================================================================
 // Test Functions
 //==================================================================
-let recipeIndex = [
-    [ 'Breakfast-Pizza', 'breakfast pizza egg spinach butter' ],
-    [ 'Breakfast-French-Toast', 'breakfast french toast egg bread cinnamon' ],
-    [ 'Bread-Pizza', 'bread pizza flour sauce spinach' ],
-    [ 'Bread-Sourdough', 'bread sourdough starter flour salt' ],
-];
+// let recipeIndex = [
+//     [ 'Breakfast-Pizza', 'breakfast pizza egg spinach butter' ],
+//     [ 'Breakfast-French-Toast', 'breakfast french toast egg bread cinnamon' ],
+//     [ 'Bread-Pizza', 'bread pizza flour sauce spinach' ],
+//     [ 'Bread-Sourdough', 'bread sourdough starter flour salt' ],
+//     [ 'Garbage-Junk', 'blah blah garbage junk blah blah garbage junk blah blah garbage junk' ],
+//     [ 'outline-container-org2f77f0c', 'english muffin sourdough' ],
+//     [ 'outline-container-org0a2d7ff', 'german muffin sourdough' ],
+// ];
 
 function searchDriver() {
     const queries = [
